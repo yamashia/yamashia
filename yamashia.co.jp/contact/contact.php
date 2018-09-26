1e7c2813bf8e5cdad3e9288b27845d7e7b54b1ca
yamashia <qyama2003@gmail.com>
2018-09-26 17:55:30 +0900

    Add files via upload

---
+++ yamashia.co.jp/contact/contact.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>株式会社ヤマシア</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="responsive.css">
+  </head>
+  <body>
+  <header>
+    <div  class="header-left"><a href="../index.php">株式会社ヤマシア</a></div>
+    <div id class="header-right">
+    <div id class="menu">
+<ul>
+<li><a href="../index.php">ホーム</a></li>
+<li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a>
+<ul>
+<li><a href="../BuisinessInfo/BuisinessInfo.php">案件支援</a></li>
+<li><a href="../BuisinessInfo/BuisinessInfo.php">案件受託</a></li>
+  </ul>
+</li>
+<li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
+<li><a href="../contact/contact.php">お問い合わせ</a></li>
+</ul>
+	</div>
+	</div>
+  <div id="nav-drawer">
+       <input id="nav-input" type="checkbox" class="nav-unshown">
+       <label id="nav-open" for="nav-input"><span></span></label>
+       <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+       <div id="nav-content" class="nav-content">
+         <ul>
+         <li><a href="../index.php">ホーム</a></li>
+         <li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a>
+         <li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
+         <li><a href="../contact/contact.php">お問い合わせ</a></li>
+         </ul></div>
+    </header>
+	<div class="top-wrapper">
+  <p> お問い合わせ</p>
+<div class="mail-contact">
+  <h2> ・メールでのお問い合わせ</h2>
+  <h4>  お問い合わせにつきましては、下記リンクよりメールにてお問い合わせください</h4>
+  <h4>  kenichi.yamaguchi@yamashia.co.jp</h4>
+</div>
+<div class="line-contact">
+  <h2> ・LINEでのお問い合わせ</h2>
+  <h4>　LINE ID: yamashia</h4>
+  <img src="../contact/line_qr.png" width="200px" height="200px">
+</div>
+<div class="contact">
+  <h2> ・採用についてのお問い合わせ</h2>
+  <h4> 新卒／中途退学問わず受け付けております。詳しくは弊社までお問い合わせください。</h4>
+  </div>
+  </body>
+
+  <footer>
+  <div class="footer-logo"><a href="../index.php">yamashia</a></div>
+  <div class="footer-list">
+    <ul>
+      <li><a href="../index.php">ホーム</a></li>
+      <li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a></li>
+      <li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
+      <li><a href="../contact/contact.php">お問い合わせ</a></li>
+    </ul>
+      </div>
+  </footer>
+</html>
