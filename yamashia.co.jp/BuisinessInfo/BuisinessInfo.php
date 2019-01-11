b3b93fa98652f8ff0206da4a0be154a587480c57
yamashia <qyama2003@gmail.com>
2019-01-11 20:47:31 +0900

    Add files via upload

--- yamashia.co.jp/BuisinessInfo/BuisinessInfo.php
+++ yamashia.co.jp/BuisinessInfo/BuisinessInfo.php
@@ -1,47 +1,58 @@
 <!DOCTYPE html>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script>
+(function($) {
+    $(function() {
+        var $header = $('#top-head');
+        // Nav Fixed
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > 350) {
+                $header.addClass('fixed');
+            } else {
+                $header.removeClass('fixed');
+            }
+        });
+        // Nav Toggle Button
+        $('#nav-toggle').click(function(){
+            $header.toggleClass('open');
+        });
+    });
+})(jQuery);
+</script>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>株式会社ヤマシア</title>
     <link rel="stylesheet" href="stylesheet.css">
-    <link rel="stylesheet" href="responsive.css">
+  	<link rel="stylesheet" href="responsive.css">
   </head>
   <body>
-  <header>
-        <div  class="header-left"><a href="../index.php">株式会社ヤマシア</a></div>
-        <div id class="header-right">
-        <div id class="menu">
-		<ul>
-   <li><a href="../index.php">ホーム</a></li>
-   <li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a>
-		<ul>
-   <li><a href="../BuisinessInfo/BuisinessInfo.php">案件支援</a></li>
-   <li><a href="../BuisinessInfo/BuisinessInfo.php">案件受託</a></li>
-	    </ul>
-   </li>
-   <li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
-   <li><a href="../contact/contact.php">お問い合わせ</a></li>
-	</ul>
-	</div>
-  </div>
-  <div id="nav-drawer">
-       <input id="nav-input" type="checkbox" class="nav-unshown">
-       <label id="nav-open" for="nav-input"><span></span></label>
-       <label class="nav-unshown" id="nav-close" for="nav-input"></label>
-       <div id="nav-content" class="nav-content">
-         <ul>
-         <li><a href="../index.php">ホーム</a></li>
-         <li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a>
-         <li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
-         <li><a href="../contact/contact.php">お問い合わせ</a></li>
-         </ul></div>
-</div>
+    <header id="top-head">
+        <div class="inner">
+            <div id="mobile-head">
+                <h1 class="logo">株式会社ヤマシア</h1>
+                <div id="nav-toggle">
+                    <div>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <nav id="global-nav">
+              <ul>
+                <li><a href="../index.php">ホーム</a></li>
+                <li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a>
+                <li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
+                <li><a href="../contact/contact.php">お問い合わせ</a></li>
+              </ul>
+            </nav>
+        </div>
     </header>
 
 	<div class="top-wrapper">
         <p>事業案内</p>
-
   <div class="Case">
 <h2>案件支援（IT支援）</h2>
 <h5>IT支援を実施します。</h5>
@@ -56,16 +67,15 @@
   </div>
   </div>
   </body>
-
   <footer>
-  <div class="footer-logo"><a href="../index.php">yamashia</a></div>
-  <div class="footer-list">
-    <ul>
-      <li><a href="../index.php">ホーム</a></li>
-      <li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a></li>
-      <li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
-      <li><a href="../contact/contact.php">お問い合わせ</a></li>
-    </ul>
-      </div>
+   <div class="footer-logo"><a href="../index.php">yamashia</a></div>
+   <div class="footer-list">
+      <ul>
+        <li><a href="../index.php">ホーム</a></li>
+        <li><a href="../BuisinessInfo/BuisinessInfo.php">事業案内</a></li>
+        <li><a href="../CompanyBrochure/CompanyBrochure.php">会社案内</a></li>
+        <li><a href="../contact/contact.php">お問い合わせ</a></li>
+      </ul>
+    </div>
   </footer>
 </html>
