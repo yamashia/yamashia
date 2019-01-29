e72068ba37286e3b3df2093c895e47ef98a85ee8
yamashia <qyama2003@gmail.com>
2019-01-29 15:23:10 +0900

    Add files via upload

--- yamashia.co.jp/contact/Confirmation.php
+++ yamashia.co.jp/contact/Confirmation.php
@@ -25,7 +25,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>株式会社ヤマシア</title>
     <link rel="stylesheet" href="../css/stylesheet.css">
   	<link rel="stylesheet" href="../css/responsive.css">
